79d0fdc8cd376c69352da5819b33dd17762e7478
Jan Palach <palach@gmail.com>
2022-11-03 18:03:22 -0300

    feat(00001): Changes the design of the class GlinblingLog

--- glinblin/glinbling.py
+++ glinblin/glinbling.py
@@ -1,12 +1,23 @@
 import logging
 from typing import Mapping
+from enum import Enum, auto
+
+
+class Severity(Enum):
+    INFO = auto()
+    DEBUG = auto()
+    WARNING = auto()
+    ERROR = auto()
+    CRITICAL = auto()
+    NOT_SET = auto()
 
 
 class GlinblinLog:
+
     def __init__(self):
         pass
 
-    def setup_from_file(self, config_file_path=None) -> Mapping:
+    def setup_from_file(self, config_file_path: str) -> Mapping:
         pass
 
     def setup_from_arguments(self) -> Mapping:
@@ -18,26 +29,5 @@ class GlinblinLog:
     def __enqueue_message(self) -> bool:
         pass
 
-    def send(self) -> None:
-        pass
-
-    def info(self, message:str=None) -> None:
-        pass
-
-    def info(self, message:str=None) -> None:
-        pass
-
-    def debug(self, message:str=None) -> None:
-        pass
-
-    def warning(self, message:str=None) -> None:
-        pass
-
-    def error(self, message:str=None) -> None:
-        pass
-
-    def critical(self, message:str=None) -> None:
-        pass
-
-    def not_set(self, message:str=None) -> None:
+    def log(self, severity: Severity, message: str) -> None:
         pass
