d82b3b07a2cec225ecc36902737fdc976e915d82
Jan Palach <palach@gmail.com>
2022-11-08 10:06:14 -0300

    feat(00001): Renames formatter to buffer and makes some adjustments in setup.py

--- setup.py
+++ setup.py
@@ -6,14 +6,14 @@ with open("README.md", "r") as fhandler:
 
 setup(
     name="glinblin",
-    version="0.0.1",
+    version="0.0.3",
     url="https://github.com/jansilva/glinblin",
     license="MIT License",
     author="Jan Palach",
     long_description="readme",
     long_description_content_type="text/markdown",
     author_email="palach@gmail.com",
-    keywords="log, aws. elasticsearch, async, python3, Linux",
+    keywords="log, aws. elasticsearch, async, python3, Linux, logger",
     description="Implements a logger formatter in order to send it to an \
         elasticsearch repository.",
     packages=["glinblin"],
